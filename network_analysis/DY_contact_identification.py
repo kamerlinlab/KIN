d777750b4cc28ee8214712aec0906dc6399bb8fe
RMCrean <RMCrean@users.noreply.github.com>
2023-06-06 14:00:38 +0100

    Linting, formatting in network_analysis folder
    No changes to actual code logic, just clean up.

--- network_analysis/DY_contact_identification.py
+++ network_analysis/DY_contact_identification.py
@@ -1,13 +1,12 @@
-from typing import Tuple, Optional, List
-import warnings
+"""
+TODO - add docstring.
+"""
+from typing import Tuple, Optional
 import pickle
-import pandas as pd
 import numpy as np
-import builtins
 
 import MDAnalysis
 from MDAnalysis import Universe
-from MDAnalysis.analysis.hydrogenbonds.hbond_analysis import HydrogenBondAnalysis as HBA
 from MDAnalysis.analysis import contacts
 
 POSITIVE_SB_RESIDUES = ("LYS", "ARG")
@@ -15,141 +14,155 @@ NEGATIVE_SB_RESIDUES = ("GLU", "ASP")
 HYDROPHOBIC_RESIDUES = ("ALA", "VAL", "LEU", "ILE", "PRO", "PHE", "Cys")
 
 
-def calculate_contacts(parm_file: str, traj_file: str, msa_sequence: str,
-		short_msa_sequence:str, out_file: str,first_res: Optional[int] = None,
-		last_res: Optional[int] = None,
-		report_timings: bool = True) -> None:
+def calculate_contacts(
+    parm_file: str,
+    traj_file: str,
+    msa_sequence: str,
+    short_msa_sequence: str,  # TODO - this is not currently used.
+    out_file: str,
+    first_res: Optional[int] = None,
+    last_res: Optional[int] = None,
+    report_timings: bool = True,
+) -> None:
+    """
+    TODO - add docstring.
+    """
+    if report_timings:
+        import time
+        from datetime import timedelta
 
-	if report_timings:
-		import time
-		from datetime import timedelta
-		start_time = time.monotonic()
-	
-	
-	universe = Universe(parm_file, traj_file)
-	all_contact_scores = {}
-	for res in universe.residues:
-		res_name = str(res.resid) + "_" + res.resname.capitalize()
-		all_contact_scores[res_name]={}
-	
-	if first_res is None:
-		first_res = 1
-	if last_res is None:
-		last_res = len(universe.atoms.residues)
-		
-	for res1 in range(first_res, last_res + 1):
-		res1_sele = "not name H* and resid " + str(res1)
-		res1_atoms = universe.select_atoms(res1_sele)
+        start_time = time.monotonic()
+
+    universe = Universe(parm_file, traj_file)
+    all_contact_scores = {}
+    for res in universe.residues:
+        res_name = str(res.resid) + "_" + res.resname.capitalize()
+        all_contact_scores[res_name] = {}
+
+    if first_res is None:
+        first_res = 1
+    if last_res is None:
+        last_res = len(universe.atoms.residues)
+
+    for res1 in range(first_res, last_res + 1):
+        res1_sele = "not name H* and resid " + str(res1)
+        res1_atoms = universe.select_atoms(res1_sele)
 
         # symmetrical matrix along diagonal, hence loop style.
-		for res2 in range(res1, len(universe.residues) + 1):
-			res_delta = abs(res1 - res2)
-			
-			res2_sele = "not name H* and resid " + str(res2)
-			res2_atoms = universe.select_atoms(res2_sele)
-			contact_scores = []
-			for timestep in universe.trajectory:
-				res_res_dists = contacts.distance_array(
-						res1_atoms.positions,
-						res2_atoms.positions)
-				contact_score = _score_residue_contact(
-						res_res_dists=res_res_dists)
-				contact_scores.append(contact_score)
-			avg_contact_score = sum(
-					contact_scores) / len(universe.trajectory)
+        for res2 in range(res1, len(universe.residues) + 1):
+            res_delta = abs(
+                res1 - res2
+            )  # TODO - use res_delta to filter out false contacts
 
-			if avg_contact_score > 0.6:
-				final_contact_score=1
-			else:
-				final_contact_score=0
+            res2_sele = "not name H* and resid " + str(res2)
+            res2_atoms = universe.select_atoms(res2_sele)
+            contact_scores = []
+            for timestep in universe.trajectory:
+                res_res_dists = contacts.distance_array(
+                    res1_atoms.positions, res2_atoms.positions
+                )
+                contact_score = _score_residue_contact(res_res_dists=res_res_dists)
+                contact_scores.append(contact_score)
+            avg_contact_score = sum(contact_scores) / len(universe.trajectory)
 
-			res1_name = universe.residues[res1-1].resname.capitalize()
-			res2_name = universe.residues[res2-1].resname.capitalize()
-			contact1_label = str(res1) + "_" + res1_name
-			contact2_label = str(res2) + "_" + res2_name 
-			all_contact_scores[contact1_label][contact2_label]=final_contact_score
-	print("done making contact scores, moved on to indexing")
+            if avg_contact_score > 0.6:
+                final_contact_score = 1
+            else:
+                final_contact_score = 0
 
+            res1_name = universe.residues[res1 - 1].resname.capitalize()
+            res2_name = universe.residues[res2 - 1].resname.capitalize()
+            contact1_label = str(res1) + "_" + res1_name
+            contact2_label = str(res2) + "_" + res2_name
+            all_contact_scores[contact1_label][contact2_label] = final_contact_score
+    print("done making contact scores, moved on to indexing")
 
-	msa_contact_scores = indexing_pdb_to_msa(msa_sequence, all_contact_scores)
+    msa_contact_scores = indexing_pdb_to_msa(msa_sequence, all_contact_scores)
 
-	#checking the msa to pdb inde conversion
-	index_contact_scores = {} 
-	for key_1, value_1 in all_contact_scores.items():
-		res_pdb_id_1, res_pdb_name_1 = key_1.split("_")
-		index_contact_scores[int(res_pdb_id_1)]={}
-		for key_2, value_2 in all_contact_scores[key_1].items():
-			res_pdb_id_2, res_pdb_name_2 = key_2.split("_")
-			index_contact_scores[int(res_pdb_id_1)][int(res_pdb_id_2)]=value_2
-		
-	pdb_contact_scores = indexing_msa_to_pdb(msa_sequence, msa_contact_scores)
+    # checking the msa to pdb inde conversion
+    index_contact_scores = {}
+    for key_1, value_1 in all_contact_scores.items():
+        res_pdb_id_1, res_pdb_name_1 = key_1.split("_")
+        index_contact_scores[int(res_pdb_id_1)] = {}
+        for key_2, value_2 in all_contact_scores[key_1].items():
+            res_pdb_id_2, res_pdb_name_2 = key_2.split("_")
+            index_contact_scores[int(res_pdb_id_1)][int(res_pdb_id_2)] = value_2
 
-	if pdb_contact_scores == index_contact_scores:
-		print("dictionaries are the same")
-	else: 
-		print("dictionaries are different")
+    pdb_contact_scores = indexing_msa_to_pdb(msa_sequence, msa_contact_scores)
 
-	#save output 
-	with open(f"{out_file}", "wb") as file:
-		pickle.dump(msa_contact_scores, file)
+    if pdb_contact_scores == index_contact_scores:
+        print("dictionaries are the same")
+    else:
+        print("dictionaries are different")
 
+    # save output
+    with open(f"{out_file}", "wb") as file:
+        pickle.dump(msa_contact_scores, file)
 
-	if report_timings:
-		end_time = time.monotonic()
-		delta_time = timedelta(seconds=end_time - start_time)
-		print(f"Time taken: {delta_time}")
+    if report_timings:
+        end_time = time.monotonic()
+        delta_time = timedelta(seconds=end_time - start_time)
+        print(f"Time taken: {delta_time}")
 
 
-	
-def indexing_pdb_to_msa(msa_sequence: list[str],
-		all_contact_scores: dict[str, dict[str,float]]) -> dict[float, float]:
-	
-	counter=0.0
-	index_pdb_msa={}
-	msa_contact_scores={}
-	missing_indicies = []
-	for i, res in enumerate(msa_sequence):
-		if res !='-':
-			counter+=1
-			index_pdb_msa[int(counter)]=i+1
-		else:
-			missing_msa_indx = i
-			missing_indicies.append(i+1)
-	print("Missing residues:", missing_indicies)
-	for key_1, value_1 in all_contact_scores.items():
-		res_pdb_id_1, res_pdb_name_1 = key_1.split("_")
-		
-		res_msa_id_1 = index_pdb_msa[int(res_pdb_id_1)]
-		msa_contact_scores[res_msa_id_1]={}
-		for key_2, value_2 in all_contact_scores[key_1].items():
-			res_pdb_id_2, res_pdb_name_2 = key_2.split("_")
-			res_msa_id_2 = index_pdb_msa[int(res_pdb_id_2)]
-			msa_contact_scores[res_msa_id_1][res_msa_id_2]=value_2
-			
-	return msa_contact_scores
+def indexing_pdb_to_msa(
+    msa_sequence: list[str], all_contact_scores: dict[str, dict[str, float]]
+) -> dict[float, float]:
+    """
+    TODO - add docstring.
+    """
+    counter = 0.0
+    index_pdb_msa = {}
+    msa_contact_scores = {}
+    missing_indicies = []
+    for i, res in enumerate(msa_sequence):
+        if res != "-":
+            counter += 1
+            index_pdb_msa[int(counter)] = i + 1
+        else:
+            missing_msa_indx = i
+            missing_indicies.append(i + 1)
+    print("Missing residues:", missing_indicies)
+    for key_1, value_1 in all_contact_scores.items():
+        res_pdb_id_1, res_pdb_name_1 = key_1.split("_")
 
-def indexing_msa_to_pdb(msa_sequence: list[str],
-		msa_contact_scores: dict[int, dict[int,float]]) -> dict[float, float]:
-	
-	counter=0
-	index_pdb_msa={}
-	for i, res in enumerate(msa_sequence):
-		if res !='-':
-			counter+=1
-			index_pdb_msa[i+1]=int(counter)
-		else:
-			index_pdb_msa[i+1] = "-"
-	pdb_contact_scores={}	
-	for key_1, value_1 in msa_contact_scores.items():
-		res_pdb_id_1 = int(index_pdb_msa[key_1])
-		pdb_contact_scores[res_pdb_id_1]={}
-		for key_2, value_2 in msa_contact_scores[key_1].items():
-			res_pdb_id_2 = int(index_pdb_msa[key_2])
-			pdb_contact_scores[res_pdb_id_1][res_pdb_id_2]=value_2
-	return pdb_contact_scores
+        res_msa_id_1 = index_pdb_msa[int(res_pdb_id_1)]
+        msa_contact_scores[res_msa_id_1] = {}
+        for key_2, value_2 in all_contact_scores[key_1].items():
+            res_pdb_id_2, res_pdb_name_2 = key_2.split("_")
+            res_msa_id_2 = index_pdb_msa[int(res_pdb_id_2)]
+            msa_contact_scores[res_msa_id_1][res_msa_id_2] = value_2
 
-def _atom_num_to_res_info(atom_num: int,
-                          universe: MDAnalysis.core.universe.Universe) -> Tuple[str, int]:
+    return msa_contact_scores
+
+
+def indexing_msa_to_pdb(
+    msa_sequence: list[str], msa_contact_scores: dict[int, dict[int, float]]
+) -> dict[float, float]:
+    """
+    TODO - add docstring.
+    """
+    counter = 0
+    index_pdb_msa = {}
+    for i, res in enumerate(msa_sequence):
+        if res != "-":
+            counter += 1
+            index_pdb_msa[i + 1] = int(counter)
+        else:
+            index_pdb_msa[i + 1] = "-"
+    pdb_contact_scores = {}
+    for key_1, value_1 in msa_contact_scores.items():
+        res_pdb_id_1 = int(index_pdb_msa[key_1])
+        pdb_contact_scores[res_pdb_id_1] = {}
+        for key_2, value_2 in msa_contact_scores[key_1].items():
+            res_pdb_id_2 = int(index_pdb_msa[key_2])
+            pdb_contact_scores[res_pdb_id_1][res_pdb_id_2] = value_2
+    return pdb_contact_scores
+
+
+def _atom_num_to_res_info(
+    atom_num: int, universe: MDAnalysis.core.universe.Universe
+) -> Tuple[str, int]:
     """
     From an MDAnalysis atom number and universe, obtain the residue number
     and residue name.
@@ -175,7 +188,6 @@ def _atom_num_to_res_info(atom_num: int,
     return res_name, resid
 
 
-
 def _score_residue_contact(res_res_dists: np.ndarray, dist_cut: float = 6.0) -> float:
     """
     Score the "strength" of a pair of residues based on their atomic distances.
@@ -198,22 +210,20 @@ def _score_residue_contact(res_res_dists: np.ndarray, dist_cut: float = 6.0) -> 
     """
     # question: should i count any distance that passes min cutoff
     # as an interaction?
-# Just set it to at least one distance be within the distance 
-#Force field per residue interaction 
- 
+    # Just set it to at least one distance be within the distance
+    # Force field per residue interaction
 
     interactions = 0
     interaction_counter = 0
     for dist in res_res_dists.flatten():
-	    if dist < dist_cut: 
-		    interactions += dist
-		    interaction_counter += 1
+        if dist < dist_cut:
+            interactions += dist
+            interaction_counter += 1
     if interaction_counter > 0:
-	    avg_interactions = interactions/interaction_counter
+        avg_interactions = interactions / interaction_counter
     else:
-	    avg_interactions = 0
+        avg_interactions = 0
     if avg_interactions != 0:
-	    return 1
+        return 1
     else:
-	    return 0
-
+        return 0
--- network_analysis/multi_kif_dict.py
+++ network_analysis/multi_kif_dict.py
@@ -1,125 +1,130 @@
-import numpy as np 
-import pandas as pd 
-from varname import nameof
+""" Add docstring """
+import sys
+import pickle
 import csv
 import statistics
-import pickle 
+import numpy as np
 import matplotlib.pyplot as plt
 
-sd_calc=False
+sd_calc = False
 
-with open("structure_names.txt", "r") as file:
-	names=file.readlines()
-names=[line.strip() for line in names]
+with open("structure_names.txt", "r", encoding="utf-8") as file:
+    names = file.readlines()
+names = [line.strip() for line in names]
 print(names)
-all_contacts={}
-max_len_sequance=("NAME", 0)
-sequence_length =[]
+all_contacts = {}
+max_len_sequence = ("NAME", 0)
+sequence_length = []
 # load sequences and select the longest one
 
 for structure in names:
-	key=f"{structure}"
-	with open(f"contacts/{structure}_msa_ind_contacts.pickle", "rb") as file:
-		contacts=pickle.load(file)
-		if len(contacts)> max_len_sequance[1]:
-			 max_len_sequance=(structure, len(contacts))
-		name_length = (structure,  len(contacts))
-		sequence_length.append(name_length)
+    key = f"{structure}"  # TODO - what does this line do?
+    with open(f"contacts/{structure}_msa_ind_contacts.pickle", "rb") as file:
+        contacts = pickle.load(file)
+        if len(contacts) > max_len_sequence[1]:
+            max_len_sequence = (structure, len(contacts))
+        name_length = (structure, len(contacts))
+        sequence_length.append(name_length)
 
-	all_contacts[key]=contacts
-print("longest sequence is:", max_len_sequance)
-print("all lengths:", sequence_length) 
+    all_contacts[key] = contacts
+print("longest sequence is:", max_len_sequence)
+print("all lengths:", sequence_length)
 
-#compare interactions present in all other sequences to the longest one
+# compare interactions present in all other sequences to the longest one
 int_strength_counter = {}
 int_presence_counter = {}
 
 for structure, value in all_contacts.items():
-	for res_1, value_1 in all_contacts[structure].items():
-		if res_1 not in int_presence_counter:
-			int_presence_counter[res_1]={}
-			int_strength_counter[res_1]={}
-		for res_2, interaction in all_contacts[structure][res_1].items():
-			if  res_2 not in int_presence_counter[res_1]:
-				int_presence_counter[res_1][res_2]=0
-			int_presence_counter[res_1][res_2]+=1
-			if (res_2 not in int_strength_counter[res_1]
-					and interaction == 1):
-				int_strength_counter[res_1][res_2]=0
-			if interaction == 1:
-				int_strength_counter[res_1][res_2]+=1
-max_interaction={}
+    for res_1, value_1 in all_contacts[structure].items():
+        if res_1 not in int_presence_counter:
+            int_presence_counter[res_1] = {}
+            int_strength_counter[res_1] = {}
+        for res_2, interaction in all_contacts[structure][res_1].items():
+            if res_2 not in int_presence_counter[res_1]:
+                int_presence_counter[res_1][res_2] = 0
+            int_presence_counter[res_1][res_2] += 1
+            if res_2 not in int_strength_counter[res_1] and interaction == 1:
+                int_strength_counter[res_1][res_2] = 0
+            if interaction == 1:
+                int_strength_counter[res_1][res_2] += 1
+max_interaction = {}
 for res_1, value_1 in int_strength_counter.items():
-	for res_2, interaction in int_strength_counter[res_1].items():
-		if interaction==len(sequence_length) and res_1!=res_2:
-			if res_1 not in max_interaction:
-				max_interaction[res_1]=1
-			else:
-				max_interaction[res_1]+=1	
+    for res_2, interaction in int_strength_counter[res_1].items():
+        if interaction == len(sequence_length) and res_1 != res_2:
+            if res_1 not in max_interaction:
+                max_interaction[res_1] = 1
+            else:
+                max_interaction[res_1] += 1
 
 
 sorted_values = sorted(max_interaction.items(), key=lambda item: item[1], reverse=True)
 
-# Identify how many of the interactions are formed in each structures 
+# Identify how many of the interactions are formed in each structures
 # based on the global index
 
 residue_counts = {}
 for protein in all_contacts.values():
-	for residue1 in protein.keys():
-		if residue1 not in residue_counts:
-			residue_counts[residue1] = 0
-		residue_counts[residue1] += 1
+    for residue1 in protein.keys():
+        if residue1 not in residue_counts:
+            residue_counts[residue1] = 0
+        residue_counts[residue1] += 1
 
-if sd_calc is True:
-	filtered_residues = [residue for residue, count in residue_counts.items() if count >= 2]
+
+if sd_calc:
+    filtered_residues = [
+        residue for residue, count in residue_counts.items() if count >= 2
+    ]
 else:
-	filtered_residues = residue_counts
+    filtered_residues = residue_counts
 interaction_counts = {residue: [] for residue in filtered_residues}
 
 residue_std_devs = {}
 for protein in all_contacts.values():
-	for residue1, inner_dict in protein.items():
-		if residue1 in filtered_residues:
-			interaction_count = sum(inner_dict.values())
-			interaction_counts[residue1].append(interaction_count)
+    for residue1, inner_dict in protein.items():
+        if residue1 in filtered_residues:
+            interaction_count = sum(inner_dict.values())
+            interaction_counts[residue1].append(interaction_count)
 
 
-with open('int_in_all_struct.pickle', 'wb') as file:
-	pickle.dump(interaction_counts, file)
+with open("int_in_all_struct.pickle", "wb", encoding="utf-8") as file:
+    pickle.dump(interaction_counts, file)
 
 field_names = list(interaction_counts.keys())
 csv_file = "int_in_all_struct.csv"
-with open(csv_file, "w", newline="") as file:
-	writer = csv.DictWriter(file, fieldnames=field_names)
-	writer.writeheader()
-	writer.writerow(interaction_counts)
+with open(csv_file, "w", newline="", encoding="utf-8") as file:
+    writer = csv.DictWriter(file, fieldnames=field_names)
+    writer.writeheader()
+    writer.writerow(interaction_counts)
 
 
-#Calculate standard deviation and do data visualization
-if sd_calc is True:
-	residue_std_devs = {residue: statistics.stdev(counts) for residue, counts in interaction_counts.items()}
-	residue_std_devs = dict(sorted(residue_std_devs.items(), key=lambda item: item[0]))
+# Calculate standard deviation and do data visualization
+if sd_calc:
+    residue_std_devs = {
+        residue: statistics.stdev(counts)
+        for residue, counts in interaction_counts.items()
+    }
+    residue_std_devs = dict(sorted(residue_std_devs.items(), key=lambda item: item[0]))
 else:
-	quit()
-# MSA values 
-data = (np.genfromtxt('pos_ranking_nostar.dat', names=True, dtype=None, encoding=None))
-scores = data['pos_scr']
-res = np.arange(1, data.shape[0]+1)
+    sys.exit(0)
+# MSA values
+data = np.genfromtxt("pos_ranking_nostar.dat", names=True, dtype=None, encoding=None)
+scores = data["pos_scr"]
+res = np.arange(1, data.shape[0] + 1)
 # Plot SD vs MSA scores
 x_values = res
 y_values_1 = scores
 y_values_2 = [residue_std_devs.get(residue, np.nan) for residue in res]
 y_values_2_norm = []
-for i in y_values_2: 
-	norm_i=i/max(y_values_2)
-	y_values_2_norm.append(norm_i)
-plt.plot(x_values, y_values_1, label='Scores')
-plt.plot(x_values, y_values_2_norm, label='Standard Deviations')
-plt.xlabel('Residue')
-plt.ylabel('MSA Values')
-plt.title('MSA Scores and Standard Deviations of Interactions Over Structures')
+for i in y_values_2:
+    norm_i = i / max(y_values_2)
+    y_values_2_norm.append(norm_i)
+plt.plot(x_values, y_values_1, label="Scores")
+plt.plot(x_values, y_values_2_norm, label="Standard Deviations")
+plt.xlabel("Residue")
+plt.ylabel("MSA Values")
+plt.title("MSA Scores and Standard Deviations of Interactions Over Structures")
 plt.legend()
 plt.grid(True)
-plt.savefig('plot.png', dpi=300)
+plt.savefig("plot.png", dpi=300)
 
-quit()
+sys.exit(0)
