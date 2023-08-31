83a3a66e2465f781c431504130743c54ce9592f9
Dariia Yehrova <dyehorova3@gatech.edu>
2023-08-31 11:03:26 -0400

    network files for pdb and msa index are added

--- contact_analysis/dynamic_contacts_processing/run_msa_network.py
+++ contact_analysis/dynamic_contacts_processing/run_msa_network.py
@@ -2,15 +2,8 @@
 and outputs a network of contacts shared among all the structures
 along with their conservation scores. """
 
-from itertools import chain
-from collections import Counter
-import glob
-import os
-import numpy as np
-import pandas as pd
-import time
+import csv 
 from tools_proj.pymol_projections import project_pymol_res_res_scores
-
 from tools_proj.msa_network import common_network
 
 
@@ -18,9 +11,19 @@ input_fiels = (
     "/Users/dariiayehorova/lk_research/tools-project/contact_analysis/dynamic_contacts_processing/msa_index_contacts/"
 )
 projection_output = "1M40_TEM-1_network.pml"
+contact_index="pdb"
 
 conservation_tem_msa, colors_int_type = common_network(
-    input_fiels, "1M40_TEM-1", contact_index="pdb", no_vdw=True, only_sc=False
+    input_fiels, "1M40_TEM-1", contact_index, no_vdw=True, only_sc=False
 )
+if contact_index == "pdb":
+    project_pymol_res_res_scores(conservation_tem_msa, projection_output, colors_int_type)
+    output_filename = 'network_tem1_pdb.csv'
+else:
+    output_filename = 'network_tem1_msa.csv'
 
-project_pymol_res_res_scores(conservation_tem_msa, projection_output, colors_int_type)
+with open(output_filename, 'w', newline='') as csvfile:
+    csv_writer = csv.DictWriter(csvfile, fieldnames=conservation_tem_msa.keys())
+    csv_writer.writeheader()
+    csv_writer.writerow(conservation_tem_msa)
+
