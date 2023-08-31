6ec84ce054b6e5a0671c98c4b7917575135aac45
Dariia Yehrova <dyehorova3@gatech.edu>
2023-08-31 16:21:20 -0400

    Redid crystal structure based shared network with a new code

---
+++ contact_analysis/static_contacts_processing/run_msa_network.py
@@ -0,0 +1,29 @@
+"""This file takes in contacts updated with the msa indexing,
+and outputs a network of contacts shared among all the structures
+along with their conservation scores. """
+
+import csv 
+from tools_proj.pymol_projections import project_pymol_res_res_scores
+from tools_proj.msa_network import common_network
+
+
+input_fiels = (
+    "/Users/dariiayehorova/lk_research/tools-project/contact_analysis/static_contacts_processing/msa_index_contacts/"
+)
+projection_output = "shared_network/1M40_TEM-1_network.pml"
+contact_index="msa"
+
+conservation_tem_msa, colors_int_type = common_network(
+    input_fiels, "1M40_TEM-1", contact_index, no_vdw=True, only_sc=False
+)
+if contact_index == "pdb":
+    project_pymol_res_res_scores(conservation_tem_msa, projection_output, colors_int_type)
+    output_filename = 'shared_network/network_tem1_pdb.csv'
+else:
+    output_filename = 'shared_network/network_tem1_msa.csv'
+
+with open(output_filename, 'w', newline='') as csvfile:
+    csv_writer = csv.DictWriter(csvfile, fieldnames=conservation_tem_msa.keys())
+    csv_writer.writeheader()
+    csv_writer.writerow(conservation_tem_msa)
+
